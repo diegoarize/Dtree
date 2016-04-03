ae5cf776586ce50343b6388b418b9f6375e75776
diegoarize <diegoarize@outlook.com>
2016-04-03 18:17:51 -0300

    Arvore ja com avaliação de modelo

--- Code/src/MachineLeaningModule/Main.java
+++ Code/src/MachineLeaningModule/Main.java
@@ -2,23 +2,46 @@ package MachineLeaningModule;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Random;
 
 import IO.ArffHandler;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ArffHandler dataSets = new ArffHandler();
-		try {
-			dataSets.setTrainingSet(dataSets.readArff("weather.numeric.arff"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(dataSets.getTrainingSet().toString());
+		Instances testSet = null, trainingSet = null;
+		
+		trainingSet = dataSets.readArff("weather.numeric.arff");
+		testSet = dataSets.readArff("weather.numeric-test.arff");
+		
+		trainingSet.setClassIndex(trainingSet.numAttributes() - 1);
+		testSet.setClassIndex(testSet.numAttributes()-1);
+		
+		dataSets.setTrainingSet(trainingSet);
+		dataSets.setTestingSet(testSet);
+		
+		
+		J48 tree = new J48();
+		
+		tree.buildClassifier(trainingSet);
+		System.out.println(tree.getCapabilities().toString());
+		System.out.println(tree.graph());
+		
+		
+		Evaluation eval = new Evaluation(trainingSet);
+		Random rand = new Random(1);
+		int folds = 10;
+		
+		eval.crossValidateModel(tree, testSet, folds, rand);
+		System.out.println(eval.toSummaryString("Evaluation results:\n", false));
+		
+		
+	    //the confusion matrix
+		System.out.println(eval.toMatrixString("=== Overall Confusion Matrix ===\n"));
 
 	}
 
